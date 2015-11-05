1e402627e09d222bb61f7c3408c7fe26a7aba004
benniuo <1962323664@qq.com>
2015-11-05 16:47:35 +0800

    多次require 模块只想同一个

--- user/learn/day02/module/getModule.js
+++ user/learn/day02/module/getModule.js
@@ -1,4 +1,12 @@
-var myModule = require("./module");
+var myModule1 = require("./module");
 
-myModule.setName("benniuo");
-myModule.sayHello();
+var myModule2 = require("./module");
+
+myModule1.setName("benniuo1");
+myModule2.setName("benniuo2");
+myModule1.sayHello(); 
+
+
+// output Hello benniuo2
+// 模块只加载了一次，
+//myModule1 ,myModule2指向同一个模块
