6ec763346c47189c7e277fcfb9d26f4e74f35948
benniuo <1962323664@qq.com>
2015-11-11 13:11:08 +0800

    增加一个路径

--- user/learn/blog/routes/index.js
+++ user/learn/blog/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hello', function(req, res, next) {
+  res.send("the is the test route");
+});
+
 module.exports = router;
