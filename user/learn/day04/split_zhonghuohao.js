7e42515a82392b4ab76d5b9f8b3c2caa86b6cc27
benniuo <1962323664@qq.com>
2015-11-13 16:40:00 +0800

    这是一个失败的想法，妈的

---
+++ user/learn/day04/split_zhonghuohao.js
@@ -0,0 +1,27 @@
+var a = [
+		[1,2,3],
+		[
+			[4,5,6],
+			[7,8,9]
+		],
+		11,
+		12,
+		13
+];
+
+//var a=[1,2,3];
+
+// var chain = a.split(']').join('').split('[');
+
+
+// // var first = field.split("]");
+// // console.log(first);
+
+// console.log(chain);    //[ '', '1', '2', 'f3', 'ab' ]
+// 						//多一个‘’
+
+// chain.shift();
+// console.log(chain);  //['1', '2', 'f3', 'ab' ]
+
+
+console.log(a.join("#"));
